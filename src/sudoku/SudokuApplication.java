409252f45a5ee2deeef27804d30062a6e14ff71e
YutaOkkotsu16 <mrinal.sharan@st-hildas.ox.ac.uk>
2025-03-08 01:36:30 +0000

    interfaces

--- src/sudoku/SudokuApplication.java
+++ src/sudoku/SudokuApplication.java
@@ -1,29 +1,31 @@
 package sudoku;
 
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class SudokuApplication extends Application {
+import java.io.IOException;
 
+/**
+ * This class is the Root Container (the thing which attends to all of the primary objects which must communicate when
+ * the program is running (a running program is called a "process").
+ */
+public class SudokuApplication extends Application {
     private IUserInterfaceContract.View uiImpl;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws IOException {
+        //Get SudokuGame object for a new game
         uiImpl = new UserInterfaceImpl(primaryStage);
-        try{
+
+        try {
             SudokuBuildLogic.build(uiImpl);
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             throw e;
         }
+    }
 
-}
-public static void main(String[] args){
-    launch(args);
-}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
